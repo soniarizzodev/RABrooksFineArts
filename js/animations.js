e987d0bbed47af3b4a75a00469150fa7cefa0e13
soniarizzodev <soniarizzo.dev@gmail.com>
2020-01-21 23:24:10 +0100

    - About page :man: - Contact form sends now emails! :email: - Cookie consent script and privacy policy :cookie: - Fancy animations all around :dizzy: - Misc improvements - Images res lowered once again

---
+++ js/animations.js
@@ -0,0 +1,48 @@
+﻿// listen for scroll event and call animate function
+document.addEventListener('scroll', animate);
+
+// check if element is in view
+function inView(element) {
+    var elementHeight = element.clientHeight;
+    if (elementHeight > 0) {
+        // get window height
+        var windowHeight = window.innerHeight;
+        // get number of pixels that the document is scrolled
+        var scrollY = window.scrollY || window.pageYOffset;
+
+        // get current scroll position (distance from the top of the page to the bottom of the current viewport)
+        var scrollPosition = scrollY + windowHeight;
+        // get element position (distance from the top of the page to the bottom of the element)
+        var elementPosition = element.getBoundingClientRect().top + scrollY + 100;
+
+        // is scroll position greater than element position? (is element in view?)
+        if (scrollPosition > elementPosition) {
+            return true;
+        }
+
+        return false;
+    }
+    else
+        return false;
+}
+
+// animate element when it is in view
+function animate() {
+    let elements = document.getElementsByClassName('animated');
+    Array.from(elements).forEach(function (element) {         
+        // is element in view?
+        if (inView(element)) {
+            // element is in view
+            element.classList.add("fadeInUp");
+        }
+    });
+}
+
+// smooth scroll upon navigation to an internal anchor
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
